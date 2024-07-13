dfe64d69a748bdb9e3b56eb6ad9f1838e126dd0b
Lysenko <lysenkoden@gmail.com>
2024-07-13 08:59:12 +0200

    initialized blog project

---
+++ src/features/posts/SinglePostPage.js
@@ -0,0 +1,33 @@
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { selectPostById } from './postsSlice';
+import PostAuthor from './PostAuthor';
+import TimeAgo from './TimeAgo';
+import ReactionButtons from './ReactionButtons';
+
+const SinglePostPage = () => {
+  //retrieve postId:
+  const { postId } = useParams();
+  const post = useSelector((state) => selectPostById(state, Number(postId)));
+
+  if (!post) {
+    return (
+      <section>
+        <h2>Post not found!</h2>
+      </section>
+    );
+  }
+
+  return (
+    <article>
+      <h2>{post.title}</h2>
+      <p>{post.body}</p>
+      <p className="postCredit">
+        <PostAuthor userId={post.userId} />
+        <TimeAgo timestamp={post.date} />
+      </p>
+    </article>
+  );
+};
+
+export default SinglePostPage;
--- src/features/posts/TimeAgo.js
+++ src/features/posts/TimeAgo.js
@@ -1,15 +1,15 @@
 import { parseISO, formatDistanceToNow } from 'date-fns';
 
-const TimeAgo = ({ timeStamp }) => {
+const TimeAgo = ({ timestamp }) => {
   let timeAgo = '';
-  if (timeStamp) {
-    const date = parseISO(timeStamp);
+  if (timestamp) {
+    const date = parseISO(timestamp);
     const timePeriod = formatDistanceToNow(date);
     timeAgo = `${timePeriod} ago`;
   }
 
   return (
-    <span title={timeStamp}>
+    <span title={timestamp}>
       &nbsp; <i>{timeAgo}</i>
     </span>
   );
