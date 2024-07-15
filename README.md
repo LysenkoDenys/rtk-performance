0f260c6a6a677b0a7b5311b2256fdd14ec84e141
Lysenko <lysenkoden@gmail.com>
2024-07-15 08:35:49 +0200

    added counter

--- README.md
+++ README.md
@@ -1,13 +1,11 @@
-# Learn RTK CRUD
+# Learn RTK CRUD increase performance
 
-### https://www.youtube.com/watch?v=-f1iAsrkyB0&t=82s
+### https://www.youtube.com/watch?v=GdOgYQzGexY
 
 <p>We are fetching the data of posts from an API</p>
 <p>We are making the CRUD</p>
 
 <p>Sample:</p>
-
-![23455](https://github.com/user-attachments/assets/5f262b63-94cd-4a8c-921f-427e15e94b96)
 
 <h5>Used API`s and libraries:</h5>
 <ul>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,31 @@
 import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { increaseCount, getCount } from '../features/posts/postsSlice';
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const count = useSelector(getCount);
   return (
     <header>
       <h1>Redux blog</h1>
       <nav>
         <ul>
           <li>
-            <Link to="/"> Home</Link>
+            <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to="post"> Post</Link>
+            <Link to="post">Post</Link>
+          </li>
+          <li>
+            <Link to="user">Users</Link>
           </li>
         </ul>
+        <button
+          className="btn-counter"
+          onClick={() => dispatch(increaseCount())}
+        >
+          {count}
+        </button>
       </nav>
     </header>
   );
